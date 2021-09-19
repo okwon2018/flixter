207f9a8ab33bd1e2422af47dc80bce27e798b19a
okwon2018 <47762109+okwon2018@users.noreply.github.com>
2021-09-19 15:03:22 -0400

    Initial Commit

---
+++ flixter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Ohsung Kwon on 9/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
